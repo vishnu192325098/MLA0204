21601637c6a3a1d7e6605799cf6dcd77b926d01d
vishnu192325098 <160741080+vishnu192325098@users.noreply.github.com>
2025-07-05 13:50:05 +0530

    Update boosting.py

--- boosting.py
+++ boosting.py
@@ -3,15 +3,12 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
 data = load_iris()
 X = data.data
 y = data.target
-
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=49
 )
-
 weak_learner = DecisionTreeClassifier(max_depth=1)
 boost_model = AdaBoostClassifier(
     estimator=weak_learner, 
@@ -19,11 +16,8 @@ boost_model = AdaBoostClassifier(
     learning_rate=1
 )
 boost_model.fit(X_train, y_train)
-
 y_pred = boost_model.predict(X_test)
-
 accuracy = accuracy_score(y_test, y_pred)
-
 print("Predicted Labels:", y_pred)
 print("Actual Labels   :", y_test)
 print("Boosting Accuracy:", accuracy)
